38a3e2e114fdd41e335ea7a78416ac34429859c0
Brent Benson <bwbensonjr@gmail.com>
2020-04-20 07:45:45 -0400

    Added matplotlib details

--- python/covid19.py
+++ python/covid19.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 ID_COLS = [
     "date",
@@ -35,4 +38,7 @@ def main():
     sv = (pd.read_csv("https://covidtracking.com/api/v1/states/daily.csv",
                       parse_dates=["date", "dateChecked"])
           .melt(id_vars=ID_COLS, value_vars=VALUE_COLS))
+
+    matplotlib.use("MacOSX")
     sns.bartplot(x="date", y="value", data=sv.query("state == 'MA'").query("variable == 'deathIncrease'"))
+    plt.show()
